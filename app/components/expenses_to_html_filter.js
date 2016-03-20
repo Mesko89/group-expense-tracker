371f525e456519f4a1a0a97e16400396019efe5f
Mesko <me@mesko.co>
2016-03-20 18:19:01 +0100

    Show amount info only for those whos amount is bigger than 0

--- app/components/expenses_to_html_filter.js
+++ app/components/expenses_to_html_filter.js
@@ -4,9 +4,12 @@ angular.module('expensesToHtml', [])
         return function (expenses, template, filler) {
             filler = filler || ', ';
             return expenses.map(function(expense) {
+                if (!expense.value) return null;
                 return template
                     .replace('{{personName}}', expense.personName)
                     .replace('{{value}}', $filter('currency')(expense.value));
+            }).filter(function(value) {
+                return value !== null;
             }).join(filler);
         };
     });
